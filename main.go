07a0e6dd05c0cc70137209707f1ff53530b039b1
weili <weili@sensorsdata.cn>
2019-11-12 14:59:25 +0800

    fix mget test

--- main.go
+++ main.go
@@ -3,24 +3,25 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"strings"
 	"time"
 )
 import "github.com/gocql/gocql"
 
 /* Before you execute the program, Launch `cqlsh` and execute:
-create keyspace example with replication = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };
-create table example.large(id int, PRIMARY KEY(id));
+create keyspace demo with replication = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };
+create table demo.large(id int, value text, PRIMARY KEY(id));
 */
 
-var msetNum = 200
+var msetNum = 100
 var mgetNum = 2000
-var randNum = 20000
+var randNum = 2000000
 
 func main() {
 	//
-	cluster := gocql.NewCluster("10.42.78.194", "10.42.29.220", "10.42.76.189")
-	cluster.Keyspace = "example"
+	cluster := gocql.NewCluster("10.42.78.194")
+	cluster.Keyspace = "demo"
 	cluster.Consistency = gocql.Quorum
 	session, _ := cluster.CreateSession()
 
@@ -32,14 +33,15 @@ func main() {
 
 func mset(session *gocql.Session) {
 	var total = int64(0)
-	for j := 0; j < 2000000; j++ {
+	for j := 9000000; j < 9500000; j++ {
 		begin := "BEGIN BATCH"
 		end := "APPLY BATCH"
 		query := ""
 		args := make([]interface{}, 0)
 		for i := 0; i < msetNum; i++ {
-			query = query + "INSERT INTO large (id) VALUES (?);"
-			args = append(args, i*j+i)
+			query = query + "INSERT INTO large (id, value) VALUES (?, ?);"
+			args = append(args, msetNum*j+i)
+			args = append(args, strconv.Itoa(msetNum*j+i))
 		}
 
 		fullQuery := strings.Join([]string{begin, query, end}, "\n")
@@ -50,7 +52,7 @@ func mset(session *gocql.Session) {
 		endTime := time.Now().UnixNano()
 		deltaTime := (endTime - startTime) / 1000000
 		total = total + deltaTime
-		s := fmt.Sprintf("MSET 第 %d 次， 时间：%d ms, 平均时间: %d ms", j, deltaTime, total/int64(j+1))
+		s := fmt.Sprintf("MSET 第 %d 次， 时间：%d ms, 平均时间: %d ms", j, deltaTime, total/int64(j+1-9000000))
 		fmt.Println(s)
 	}
 }
@@ -61,29 +63,31 @@ func mget(session *gocql.Session) {
 		query := ""
 		args := make([]interface{}, 0)
 		for i := 0; i < mgetNum; i++ {
-			n := RandInt64(1400000000, 1700000000)
+			n := RandInt64(900000000, 900002800)
 			var qs = ""
 			if i == 0 {
-				qs = fmt.Sprintf("SELECT id FROM example.large WHERE id in (?")
+				qs = fmt.Sprintf("SELECT id, value FROM demo.large WHERE id in (?")
 			} else {
 				qs = ",?"
 			}
 			query = query + qs
 			args = append(args, n)
 		}
-		query = query + ");"
-		fullQuery := strings.Join([]string{"", query, ""}, "\n")
+		fullQuery := query + ");"
+
+		var id int
+		var value string
+
 		startTime := time.Now().UnixNano()
 		iter := session.Query(fullQuery, args...).Iter()
-		var id int
-		for iter.Scan(&id) {
+
+		for iter.Scan(&id, &value) {
 		}
 		endTime := time.Now().UnixNano()
 		deltaTime := (endTime - startTime) / 1000000
 		total = total + deltaTime
 		s := fmt.Sprintf("MGET 第 %d 次， 时间：%d ms, 平均时间: %d ms", j, deltaTime, total/int64(j))
 		fmt.Println(s)
-		time.Sleep(time.Second * 1)
 	}
 
 }
