e21c7a5f918a45cb2811a50ffc2e2ab622c6dc31
weili <weili@sensorsdata.cn>
2019-11-19 15:12:22 +0800

    change mset

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 import "github.com/gocql/gocql"
@@ -17,23 +18,36 @@ create table demo.kv(id int, value text, PRIMARY KEY(id));
 var msetNum = 200
 var mgetNum = 2000
 var randNum = 2000000
-
+var goNums = 20
 func main() {
 	//
-	cluster := gocql.NewCluster("10.42.78.194")
+	cluster := gocql.NewCluster("10.42.68.17", "10.42.165.220", "10.42.169.144")
 	cluster.Keyspace = "demo"
 	cluster.Consistency = gocql.Quorum
 	session, _ := cluster.CreateSession()
-
-	defer session.Close()
-	//mset(session)
-	mget(session)
+	sessions := make([]*gocql.Session, 0)
+	for i := 0; i < goNums; i++{
+		session1, e1 := cluster.CreateSession()
+		if e1 != nil {
+			fmt.Println("err", e1)
+		}
+		fmt.Println("session = ", session1)
+		sessions = append(sessions, session1)
+	}
+	defer func() {
+		session.Close()
+		for _, s := range sessions{
+			s.Close()
+		}
+	}()
+	mset(session)
+	//mget(sessions)
 
 }
 
 func mset(session *gocql.Session) {
 	var total = int64(0)
-	for j := 9000000; j < 9500000; j++ {
+	for j := 0; j < 250000; j++ {
 		begin := "BEGIN BATCH"
 		end := "APPLY BATCH"
 		query := ""
@@ -54,53 +68,62 @@ func mset(session *gocql.Session) {
 		total = total + deltaTime
 		s := fmt.Sprintf("MSET 第 %d 次， 时间：%d ms, 平均时间: %d ms", j, deltaTime, total/int64(j+1-9000000))
 		fmt.Println(s)
-		time.Sleep(time.Second * 4)
 	}
 }
 
-func mget(session *gocql.Session) {
+func mget(sessions []*gocql.Session) {
 	var total = int64(0)
 	for j := 1; j <= randNum; j++ {
-		query := ""
-		args := make([]interface{}, 0)
-		for i := 0; i < mgetNum; i++ {
-			n := RandInt(0, 50000000)
-			fmt.Println("rand n =", n)
-			var qs = ""
-			if i == 0 {
-				qs = fmt.Sprintf("SELECT id, value FROM demo.kv WHERE id in (?")
-			} else {
-				qs = ",?"
-			}
-			query = query + qs
-			args = append(args, n)
-		}
-		fullQuery := query + ");"
-
-		var id int
-		var value string
-
+		wg := sync.WaitGroup{}
 		startTime := time.Now().UnixNano()
-		iter := session.Query(fullQuery, args...).Iter()
-
-		for iter.Scan(&id, &value) {
-			fmt.Print("id, value = ", id, value)
+		fmt.Println("startTime = ", startTime)
+		for c := 0; c < goNums; c ++ {
+			session := sessions[c]
+			wg.Add(1)
+			go batchGet(session, &wg)
 		}
+		wg.Wait()
 		endTime := time.Now().UnixNano()
+		fmt.Println("endTime = ", endTime)
 		deltaTime := (endTime - startTime) / 1000000
 		total = total + deltaTime
 		s := fmt.Sprintf("MGET 第 %d 次， 时间：%d ms, 平均时间: %d ms", j, deltaTime, total/int64(j))
 		fmt.Println(s)
 		time.Sleep(time.Second * 5)
 	}
-
 }
 
 
 func RandInt(min, max int) int {
 	rand.Seed(time.Now().UnixNano())
-	if min >= max {
-		return max
-	}
 	return rand.Intn(max-min) + min
+}
+
+func batchGet(session *gocql.Session, group *sync.WaitGroup, ) {
+	defer group.Done()
+	query := ""
+	args := make([]interface{}, 0)
+	for i := 0; i < mgetNum/goNums; i++ {
+		n := RandInt(0, 50000000)
+		var qs = ""
+		if i == 0 {
+			qs = fmt.Sprintf("SELECT id, value FROM demo.kv WHERE id in (?")
+		} else {
+			qs = ",?"
+		}
+		query = query + qs
+		args = append(args, n)
+	}
+	fullQuery := query + ");"
+
+	var id int
+	var value string
+
+	iter := session.Query(fullQuery, args...).Iter()
+	count := 0
+
+	for iter.Scan(&id, &value) {
+		count = count + 1
+	}
+	fmt.Println("count = ", count)
 }
